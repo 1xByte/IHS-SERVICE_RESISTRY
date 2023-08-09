c32054b0a1f0a903d948a768c425b08bfdabadc0
Abhishek <abhishek.panda02@outlook.com>
2023-08-09 17:02:57 +0530

    First One

---
+++ src/test/java/com/service/resistry/ServiceResistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.resistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceResistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
